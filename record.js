7100c866c2abd98529cec4a4de0202853ae18c49
devneko <dotneet@gmail.com>
2017-09-08 03:39:50 +0900

    fix lint errors

--- record.js
+++ record.js
@@ -7,10 +7,10 @@ const fs = require('fs')
 
 const args = process.argv
 
-var additionalParams = null
-if ( args.length > 2 ) {
-  var file = args[2];
-  var json = fs.readFileSync(file);
+let additionalParams = null
+if (args.length > 2) {
+  let file = args[2]
+  let json = fs.readFileSync(file)
   additionalParams = JSON.parse(json)
 }
 
@@ -20,40 +20,42 @@ async function onReceive (params) {
   console.log('receive:', params)
   let data = params[0]
 
-  if ( data.cmd === "prepare" ) {
-    if ( additionalParams !== null ) {
-      var s = "window.store.state.batchParams = JSON.parse('" + JSON.stringify(additionalParams) + "')";
+  if (data.cmd === 'prepare') {
+    if (additionalParams !== null) {
+      let s = 'window.store.state.batchParams = JSON.parse(\'' + JSON.stringify(additionalParams) + '\')'
       await chromy.evaluate(s)
     }
     return additionalParams
-  } else if ( data.cmd === "initialized" ) {
-    await chromy.evaluate(function() {
-      $('#btn-record').click();
+  } else if (data.cmd === 'initialized') {
+    await chromy.evaluate(function () {
+      document.getElementById('btn-record').click()
     })
-  } else if ( data.cmd === "script_onload" ) {
+  } else if (data.cmd === 'script_onload') {
     // do nothing.
-  } else if ( data.cmd === "exit" ) {
-    console.log("exit");
+  } else if (data.cmd === 'exit') {
+    console.log('exit')
     await chromy.close()
   } else {
-    console.log("unknown command");
-    console.log(data);
+    console.log('unknown command')
+    console.log(data)
   }
 }
 
 chromy.chain()
-      .console(msg => {
-        console.log(msg)
-      })
-      .goto('http://localhost:8000/index.html')
-      .sleep(100)
-      .receiveMessage(onReceive)
-      .sleep(100)
-      .evaluate(async _ => {
-        await window.onBatch()
-      })
-      .sleep(4000)
-      .end()
-      .then(_ => chromy.close())
-      .catch(e => {console.log(e); chromy.close()})
-
+  .console(msg => {
+    console.log(msg)
+  })
+  .goto('http://localhost:8000/index.html')
+  .sleep(100)
+  .receiveMessage(onReceive)
+  .sleep(100)
+  .evaluate(async _ => {
+    await window.onBatch()
+  })
+  .sleep(4000)
+  .end()
+  .then(_ => chromy.close())
+  .catch(e => {
+    console.log(e)
+    chromy.close()
+  })
